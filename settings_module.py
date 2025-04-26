7b32fc168a46c1fe381dbcb64670db178b1bbd98
Anushree61234 <anushreerajput207@gmail.com>
2025-04-26 22:38:06 +0530

    Alien edit

--- settings_module.py
+++ settings_module.py
@@ -17,5 +17,12 @@ class Settings:
         self.bullet_color = (255,0,0)
         self.bullet_allowed_in_group = 5
 
+        #Alien settings 
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        #fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
+
 
         
